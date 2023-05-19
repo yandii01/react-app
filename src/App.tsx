58521489a7a304a4372b3e1b0cbb5050cad56a43
yandii01 <yandip123@gmail.com>
2023-05-19 16:36:37 +0700

    setting config tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,13 @@ import ListGroup from "./components/ListGroup";
 import Users from "./components/Users";
 import ApexChart from "./components/ApexChart";
 import PieChart from "./components/PieChart";
+import Home from "./components/Home";
 
 function App() {
   const [alertVisible, setAlertVisibility] = useState(false);
-  
+
   return (
-    <div>
-      {/* { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
-      <Button onClick={() => setAlertVisibility(true)}>My Button</Button> */}
-      {/* <Users></Users> */}
-      <PieChart></PieChart>
-      <ApexChart></ApexChart>
-    </div>
+    <Home></Home>
   );
 }
 
